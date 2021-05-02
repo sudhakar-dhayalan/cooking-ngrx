09321578adbc20d0b5a1a76ee5921dbdbbbf783c
sudhakar-dhayalan <sudhakar.dayalan95@gmail.com>
2021-05-02 08:10:45 +0530

    uncommented

--- src/app/stepper/consumer/consumer.component.ts
+++ src/app/stepper/consumer/consumer.component.ts
@@ -1,35 +1,35 @@
-// import { Component, OnInit } from '@angular/core';
-// import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-// import { MatDialog } from '@angular/material/dialog';
-// import { DialogComponent } from '../dialog/dialog.component';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from '../dialog/dialog.component';
 
-// @Component({
-//   selector: 'app-consumer',
-//   templateUrl: './consumer.component.html',
-//   styleUrls: ['./consumer.component.css']
-// })
-// export class ConsumerComponent implements OnInit {
+@Component({
+  selector: 'app-consumer',
+  templateUrl: './consumer.component.html',
+  styleUrls: ['./consumer.component.css']
+})
+export class ConsumerComponent implements OnInit {
 
-//   insurerForm: FormGroup;
-//   constructor(private fb: FormBuilder, private dialog: MatDialog) { }
+  insurerForm: FormGroup;
+  constructor(private fb: FormBuilder, private dialog: MatDialog) { }
 
-//   ngOnInit() {
-//     this.insurerForm = this.fb.group({
-//       name: ["", Validators.required],
-//       dob: ["", Validators.required],
-//       gender: ["", Validators.required],
-//     })
-//   }
+  ngOnInit() {
+    this.insurerForm = this.fb.group({
+      name: ["", Validators.required],
+      dob: ["", Validators.required],
+      gender: ["", Validators.required],
+    })
+  }
 
-//   onSumbit() {
-//     console.log(this.insurerForm);
-//   }
+  onSumbit() {
+    console.log(this.insurerForm);
+  }
 
-//   openDialog() {
-//     let dialogRef = this.dialog.open(DialogComponent);
-//     dialogRef.afterClosed().subscribe(result => {
-//       console.log(result);
-//     })
-//   }
+  openDialog() {
+    let dialogRef = this.dialog.open(DialogComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(result);
+    })
+  }
 
-// }
+}
